b03f3221e340255dd763fc3e9f068732523062b7
Kory Hurst <koryhurst@gmail.com>
2019-08-19 19:56:37 -0700

    Get the First level links

--- WebScraper.py
+++ WebScraper.py
@@ -10,15 +10,17 @@ url = 'https://members.nanaimochamber.bc.ca/list/'
 
 # Connect to the URL
 response = requests.get(url)
+#print(response.text)
 
-print(response)
 # Parse HTML and save to BeautifulSoup object¶
-#soup = BeautifulSoup(response.text, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
 
 # To download the whole data set, let's do a for loop through all a tags
-#for i in range(36,len(soup.findAll('a'))+1): #'a' tags are for links
-#    one_a_tag = soup.findAll('a')[i]
-#    link = one_a_tag['href']
+for i in range(36, len(soup.findAll('a')),1): #'a' tags are for links
+    one_a_tag = soup.findAll('a')[i]
+    link = one_a_tag['href']
+    if "searchalpha" in link:
+        print(link)
 #    download_url = 'http://web.mta.info/developers/'+ link
 #    urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')+1:]) 
 #    time.sleep(1) #pause the code for a sec
