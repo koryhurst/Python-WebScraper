a2dfe247e87eeb2ff3e42cf769f16fba13ce3b60
Kory Hurst <koryhurst@gmail.com>
2019-08-20 21:58:06 -0700

    Cleaned Output

--- WebScraper.py
+++ WebScraper.py
@@ -64,18 +64,62 @@ for IndexPage in LinksToIndexPages:
   #print(IndexPage)
   LinksToBusinesPages = GetLinkList(IndexPage,'t/member/')  
   for BusinessLink in RemoveDupesFromList(LinksToBusinesPages):
-    #print(BusinessLink)
+    print(BusinessLink)
+    BusinessID = BusinessLink[BusinessLink.rfind("-", 0)+1:len(BusinessLink)]
+    print(BusinessID)
     BusinessSoup = GetSoup(BusinessLink)
     #ShowPrettySoup(BusinessLink)
-    print(BusinessSoup.find_all("h1", class_="gz-pagetitle"))
-    print(BusinessSoup.find_all("span", class_="gz-cat"))
-    print(BusinessSoup.find_all("span", class_="gz-street-address"))
-    print(BusinessSoup.find_all("span", class_="gz-address-city"))
-    print(BusinessSoup.find_all(itemprop="addressRegion"))
-    print(BusinessSoup.find_all(itemprop="postalCode"))
-    print(BusinessSoup.find_all(itemprop="telephone"))
-    print(BusinessSoup.find_all(itemprop="faxNumber"))
-    BusinessUrlMessy = str(BusinessSoup.find_all(itemprop="url", class_="card-link")[0])
-    #print(type(BusinessUrlMessy))
-    print(BusinessUrlMessy[27:BusinessUrlMessy.find("itemprop",0)-1])
-    #print(BusinessUrlMessy[0])
+    BusinessName = BusinessSoup.find_all("h1", class_="gz-pagetitle")[0].getText()
+    print(BusinessName)
+
+    if len(BusinessSoup.find_all("span", class_="gz-cat"))>0:
+      BusinessCat = BusinessSoup.find_all("span", class_="gz-cat")[0].getText()
+    else:
+      BusinessCat = "N/A"    
+    print(BusinessCat)
+
+    if len(BusinessSoup.find_all("span", class_="gz-street-address"))>0:
+      StreetAddress = BusinessSoup.find_all("span", class_="gz-street-address")[0].getText()
+    else:
+      StreetAddress = "N/A"
+    print(StreetAddress)
+
+    if len(BusinessSoup.find_all("span", class_="gz-address-city"))>0:
+      City = BusinessSoup.find_all("span", class_="gz-address-city")[0].getText()
+    else:
+      City = "N/A"
+    print(City)
+
+    if len(BusinessSoup.find_all(itemprop="addressRegion"))>0:
+      Province = BusinessSoup.find_all(itemprop="addressRegion")[0].getText()
+    else:
+      Province = "N/A"
+    print(Province)
+
+    if len(BusinessSoup.find_all(itemprop="postalCode"))>0:
+      PostalCode = BusinessSoup.find_all(itemprop="postalCode")[0].getText()
+    else:
+      PostalCode = "N/A"
+    print(PostalCode)
+    
+    if len(BusinessSoup.find_all(itemprop="telephone"))>0:
+      Telephone = BusinessSoup.find_all(itemprop="telephone")[0].getText()
+    else:
+      Telephone = "N/A"
+    print(Telephone)
+    
+    if len(BusinessSoup.find_all(itemprop="faxNumber"))>0:
+      FaxNumber = BusinessSoup.find_all(itemprop="faxNumber")[0].getText()
+    else:
+      FaxNumber = "N/A"
+    print(FaxNumber)
+    
+    if len(BusinessSoup.find_all(itemprop="url", class_="card-link"))>0:
+      BusinessUrlMessy = str(BusinessSoup.find_all(itemprop="url", class_="card-link")[0])
+      BusinessUrl = BusinessUrlMessy[27:BusinessUrlMessy.find('"',28)]
+    else:
+      BusinessUrl = "N/A"
+    print(BusinessUrl)
+    
+    print("--------------------------------------------")
+    
