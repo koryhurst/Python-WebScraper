79a0cb77bd258fe5b9094e4fffdc920437ad28fe
Kory Hurst <koryhurst@gmail.com>
2019-08-20 20:01:52 -0700

    Get The Business Data

--- WebScraper.py
+++ WebScraper.py
@@ -22,6 +22,21 @@ def GetLinkList(UrlToSearch, SearchTerm): #or searchtermSSSS at some point
       linklist.append(link)
   return linklist
 
+def GetSoup(UrlToGet):
+  response = requests.get(UrlToGet)
+  #print(response.text)
+  # Parse HTML and save to BeautifulSoup object¶
+  soup = BeautifulSoup(response.text, "html.parser")
+  return soup
+  
+def SearchSoup(UrlToSearch, SearchTerm): #or searchtermSSSS at some point
+  # Connect to the URL
+  response = requests.get(UrlToSearch)
+  #print(response.text)
+  # Parse HTML and save to BeautifulSoup object¶
+  soup = BeautifulSoup(response.text, "html.parser")
+  return soup.findAll(SearchTerm)
+
 def ShowPrettySoup(UrlToShow):
   response = requests.get(UrlToShow)
   #print(response.text)
@@ -41,21 +56,26 @@ def ShowLinks(UrlToShow):
 #List Functions
 def RemoveDupesFromList(ListToClean):
   return list(dict.fromkeys(ListToClean))
-  
+
+
 
 LinksToIndexPages = GetLinkList('https://members.nanaimochamber.bc.ca/list/','searchalpha')
 for IndexPage in LinksToIndexPages:
   #print(IndexPage)
-  LinksToBusinesPages = GetLinkList(IndexPage,'t/member/')
-  
+  LinksToBusinesPages = GetLinkList(IndexPage,'t/member/')  
   for BusinessLink in RemoveDupesFromList(LinksToBusinesPages):
-    print(BusinessLink)
-
-#<a href="https://members.nanaimochamber.bc.ca/list/member/edgewood-232" target="_blank" alt="Edgewood">
-#                <span class="gz-img-placeholder">Edgewood</span>
-#            </a>
-			
-
-
-#ShowMeLinks(LinksToIndexPages[0])
-#ShowMePrettySoup(LinksToIndexPages[0])
+    #print(BusinessLink)
+    BusinessSoup = GetSoup(BusinessLink)
+    #ShowPrettySoup(BusinessLink)
+    print(BusinessSoup.find_all("h1", class_="gz-pagetitle"))
+    print(BusinessSoup.find_all("span", class_="gz-cat"))
+    print(BusinessSoup.find_all("span", class_="gz-street-address"))
+    print(BusinessSoup.find_all("span", class_="gz-address-city"))
+    print(BusinessSoup.find_all(itemprop="addressRegion"))
+    print(BusinessSoup.find_all(itemprop="postalCode"))
+    print(BusinessSoup.find_all(itemprop="telephone"))
+    print(BusinessSoup.find_all(itemprop="faxNumber"))
+    BusinessUrlMessy = str(BusinessSoup.find_all(itemprop="url", class_="card-link")[0])
+    #print(type(BusinessUrlMessy))
+    print(BusinessUrlMessy[27:BusinessUrlMessy.find("itemprop",0)-1])
+    #print(BusinessUrlMessy[0])
