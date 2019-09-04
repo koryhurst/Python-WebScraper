2f7c25d37eeaadb956eaa135367a7702529d4052
Kory Hurst <koryhurst@gmail.com>
2019-09-04 14:51:19 -0700

    Copied the resulting data to a local mysql database

---
+++ MakeTable.sql
@@ -0,0 +1,13 @@
+create table tblNanaimoBusiness
+(
+BusinessID int,
+BusinessName varchar(100),
+BusinessCategory varchar(40),
+Address varchar(60),
+City varchar(50),
+Province varchar(20),
+PostalCode char(7),
+Phone varchar (40),
+Fax varchar (20),
+WebSite varchar (120)
+);
