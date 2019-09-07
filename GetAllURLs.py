177c15c32e3c0732b2f5e8937bb4dc04a70c0e82
Kory Hurst <koryhurst@gmail.com>
2019-09-07 11:07:48 -0700

    Added Variable for Database Connection
    Also passed that connection to BeautifulSoupFunctions

--- GetAllURLs.py
+++ GetAllURLs.py
@@ -3,27 +3,28 @@ import sys
 sys.path.append('../Python-FunctionLibrary')
 import BeautifulSoupFunctions
 
+DatabaseDetails = ["localhost","root","","dbkhurst"]
+
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="",
-  database="dbkhurst"
-)
+  host=DatabaseDetail[1],
+  user=DatabaseDetail[2],
+  passwd=DatabaseDetail[3],
+  database=DatabaseDetail[4]
+  )
+
+mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 mycursor.execute("SELECT BusinessID, Website FROM tblbusiness where left(BusinessName,1) = 'a'")
 myresult = mycursor.fetchall()
 
-sql = "INSERT INTO tblwebpage (BusinessID, URL) VALUES (%s, %s)"
-AllLinks = []
-  
 for BaseURL in myresult:
   #Just skipping it for now
   if BaseURL[1] != "http://www.ablerecognition.com":
     print(BaseURL[1])
     #wait = input("PRESS ENTER TO CONTINUE")
     BeautifulSoupFunctions.ShowLinks(BaseURL[1])
-    AllUrlsOnASite = BeautifulSoupFunctions.GetSiteMap(BaseURL[1])
+    AllUrlsOnASite = BeautifulSoupFunctions.GetSiteMap(BaseURL[1], DatabaseDetails)
     for URL in AllUrlsOnASite:
       AllLinks.append((BaseURL[0],URL))
     print(AllLinks)
