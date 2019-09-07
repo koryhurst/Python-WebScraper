2a4e884b563625118deb73c1f289c0d473df34eb
Kory Hurst <koryhurst@gmail.com>
2019-09-07 11:14:40 -0700

    Syntax fixes

--- GetAllURLs.py
+++ GetAllURLs.py
@@ -6,13 +6,11 @@ import BeautifulSoupFunctions
 DatabaseDetails = ["localhost","root","","dbkhurst"]
 
 mydb = mysql.connector.connect(
-  host=DatabaseDetail[1],
-  user=DatabaseDetail[2],
-  passwd=DatabaseDetail[3],
-  database=DatabaseDetail[4]
+  host=DatabaseDetails[0],
+  user=DatabaseDetails[1],
+  passwd=DatabaseDetails[2],
+  database=DatabaseDetails[3]
   )
-
-mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 mycursor.execute("SELECT BusinessID, Website FROM tblbusiness where left(BusinessName,1) = 'a'")
@@ -23,11 +21,11 @@ for BaseURL in myresult:
   if BaseURL[1] != "http://www.ablerecognition.com":
     print(BaseURL[1])
     #wait = input("PRESS ENTER TO CONTINUE")
-    BeautifulSoupFunctions.ShowLinks(BaseURL[1])
+    #BeautifulSoupFunctions.ShowLinks(BaseURL[1])
     AllUrlsOnASite = BeautifulSoupFunctions.GetSiteMap(BaseURL[1], DatabaseDetails)
     for URL in AllUrlsOnASite:
       AllLinks.append((BaseURL[0],URL))
-    print(AllLinks)
+    #print(AllLinks)
     mycursor.executemany(sql, AllLinks)
     mydb.commit()
     AllLinks.clear()
